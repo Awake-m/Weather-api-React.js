9ae0935889ff07ce2f507f5a6a8528347d7f78b9
Awake-m <jagrutimakwana124@gmail.com>
2024-06-20 11:55:09 +0530

    project-3

--- project-3/src/Componet/Weather.jsx
+++ project-3/src/Componet/Weather.jsx
@@ -37,7 +37,6 @@ const Weather = () => {
         description: data.weather[0].description,
         temperature: data.main.temp,
         humidity: data.main.humidity,
-        pressure: data.main.pressure,
         windSpeed: data.wind.speed
       });
       setError(null);
@@ -76,23 +75,52 @@ const Weather = () => {
       {error && <p className="error-message">{error}</p>}
       {weather && (
         <div className="weather-info">
-          <p>City:<br/> {city}</p>
-          <p>Description:<br/> {weather.description} </p>
-          <p>Temperature: {weather.temperature}°C</p>
-          <p>Humidity:<br/> {weather.humidity}%</p>
-          <p>Pressure:<br/> {weather.pressure} hPa</p>
-          <p>Wind Speed:<br/> {weather.windSpeed} m/s</p>
+          <p>City:<br/><i class="fa-solid fa-city"></i><br/> {city}</p>
+          <p>Description:<br/><i class="fa-solid fa-audio-description"></i> <br/>{weather.description} </p>
+          <p>Temperature:<br/><i class="fa-solid fa-temperature-high"></i><br/>{weather.temperature}°C</p>
+          <p>Humidity:<br/><i class="bi bi-droplet-half"></i> <br/>{weather.humidity}%</p>
+          <p>Wind Speed:<br/><i class="fa-solid fa-wind"></i><br/>{weather.windSpeed} m/s</p>
         </div>
       )}
 
       <h4>Coudly condition form 5pm-7pm with,,<br/>partly cloudy condition expected at 7pm.</h4><br/>
-      <hr></hr><br/>
+      <hr></hr>
       <div className='box3'>
-        <div className='box33'></div>
-        <div className='box33'></div>
-        <div className='box33'></div>
-        <div className='box33'></div>
-        <div className='box33'></div>
+        <div className='box33'>
+          <h5>Mon</h5>
+          <i class="fa-solid fa-cloud" style={{color: 'skyblue', marginTop : '10px'}}></i>
+          <h5>22°</h5>
+        </div>
+        <div className='box33'>
+          <h5>Tue</h5>
+        <i class="fa-solid fa-cloud-sun" style={{color: 'orange', marginTop : '10px'}}></i>
+        <h5>30°</h5>
+        </div>
+        <div className='box33'>
+          <h5>Wed</h5>
+        <i class="fa-solid fa-cloud-rain" style={{color: 'skyblue', marginTop : '10px'}}></i>
+        <h5>20°</h5>
+        </div>
+        <div className='box33'>
+          <h5>Thu</h5>
+        <i class="fa-solid fa-cloud-showers-water" style={{color: 'skyblue', marginTop : '10px'}}></i>
+        <h5>21°</h5>
+        </div>
+        <div className='box33'>
+          <h5>Fri</h5>
+          <i class="fa-solid fa-cloud-bolt" style={{color: 'skyblue', marginTop : '10px'}}></i>
+          <h5>19°</h5>
+        </div>
+        <div className='box33'>
+          <h5>Sat</h5>
+          <i class="fa-solid fa-cloud-sun-rain" style={{color: 'skyblue', marginTop : '10px'}}></i>
+          <h5>23°</h5>
+        </div>
+        <div className='box33'>
+          <h5>Sun</h5>
+          <i class="fa-solid fa-sun" style={{color: 'orange', marginTop : '10px'}}></i>
+          <h5>29°</h5>
+        </div>
       </div>
       
     </div>
